1ae9a9d5daf1a5d5b0c0d3b54f851564325c5bac
yjh1008 <exesea12@gmail.com>
2024-01-14 23:34:13 +0900

    feat: 로딩스피너 구현 및 컨텐츠 api 연결

---
+++ vue-tli/src/components/posts/PostListItem.vue
@@ -0,0 +1,19 @@
+<template>
+	<li>
+		<div class="post-title">{{ props.note.title }}</div>
+		<div class="post-contents">{{ props.note.contents }}</div>
+		<div class="post-time">{{ props.note.createdAt }}</div>
+	</li>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+	note: {
+		type: Object,
+		requred: true,
+	},
+});
+</script>
+
+<style lang="scss" scoped></style>
--- vue-tli/src/views/MainPage.vue
+++ vue-tli/src/views/MainPage.vue
@@ -1,20 +1,28 @@
 <template>
 	<div>
 		<h1>Main</h1>
+		<LoadingSpinner v-if="isLoading" />
+		<ul v-else>
+			<PostListItem v-for="n in notes" :key="n._id" :note="n" />
+		</ul>
 	</div>
 </template>
 
 <script setup>
 import { fetchPosts } from '@/api';
 import { ref, onMounted } from 'vue';
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
 const notes = ref([]);
-
+const isLoading = ref(false);
 onMounted(() => {
 	fetchNotes();
 });
 
 const fetchNotes = async () => {
+	isLoading.value = true;
 	const { data } = await fetchPosts();
+	isLoading.value = false;
 	notes.value = data.posts;
 };
 </script>
